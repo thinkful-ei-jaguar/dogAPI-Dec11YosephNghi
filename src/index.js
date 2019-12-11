5d71bce65c718de38dd4c5769aa6393b237d9903
Nghi Tran <nghi.t.tran8@gmail.com>
2019-12-11 11:50:46 -0800

    updated drill 2

--- src/index.js
+++ src/index.js
@@ -1,43 +1,38 @@
 import $ from 'jquery';
 
-function creatHTML () {
-    
+function createHTML(website) {
+  // Append image element to UL
+  $('.results').append(`
+    <li><img src="${website}" width="200px"/></li>
+  `);
 }
 
-function getDogImage(num, displayDog) {
-    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
-      .then(response => response.json())
-      .then(responseJson => {
-        console.log(responseJson);
-        for(let i = 0; i < num; i++) {
-            responseJson.message[i];
-        }
-     
-
-    
-    } 
-        
-    );
-  }
-
-  function listenToInput() {
-    $('.js-search-form').submit(event => {
-      event.preventDefault();
-      let num = $(input).val();
-      getDogImages(num, displayDogSearchData);
+function getDogImages(num) {
+  // fetches # of images
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+  // Parse json body
+    .then(response => response.json())
+    // print # of images
+    .then(responseJson => {
+      for(let i = 0; i < num; i++) {
+        createHTML(responseJson.message[i]);
+      }
     });
-  }
-  
-  $(function() {
-    console.log('App loaded! Waiting for submit!');
-    listenToInput();
-  }); 
+}
 
-  function displayDogSearchData(data) {
-    const results = data.message.map((item, index) => displayResults(item));
-    $('.js-results').html(results);
-    
-    $('.results').removeClass('hidden');
-  }
+function listenToInput() {
+  // listens to when a submit takes place
+  $('form').submit(event => {
+    // prevents page from refreshing when submit
+    event.preventDefault();
+    // saves input
+    let num = $('input').val();
+    // print out # of images
+    getDogImages(num);
+  });
+}
 
-  
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  listenToInput();
+}); 
