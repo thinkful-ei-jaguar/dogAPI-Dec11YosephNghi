326ae715bdfc052c713ab8dd4664a7ca913c3569
Yoseph Student <blsck1024@gmail.com>
2019-12-11 13:30:06 -0600

    did some js..

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 import $ from 'jquery';
 
+function creatHTML () {
+    
+}
 
-function getDogImage() {
-    fetch('https://dog.ceo/api/breeds/image/random/3')
+function getDogImage(num, displayDog) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
       .then(response => response.json())
-      .then(responseJson => console.log(responseJson));
+      .then(responseJson => {
+        console.log(responseJson);
+        for(let i = 0; i < num; i++) {
+            responseJson.message[i];
+        }
+     
+
+    
+    } 
+        
+    );
   }
-  
-  function watchForm() {
-    $('form').submit(event => {
+
+  function listenToInput() {
+    $('.js-search-form').submit(event => {
       event.preventDefault();
-      getDogImage();
+      let num = $(input).val();
+      getDogImages(num, displayDogSearchData);
     });
   }
   
   $(function() {
     console.log('App loaded! Waiting for submit!');
-    watchForm();
-  });
+    listenToInput();
+  }); 
+
+  function displayDogSearchData(data) {
+    const results = data.message.map((item, index) => displayResults(item));
+    $('.js-results').html(results);
+    
+    $('.results').removeClass('hidden');
+  }
+
+  
